8952a95254d69d409581f7cc70b5d74199c93785
maz47zu <cravx9@gmail.com>
2022-05-11 22:32:36 +0200

    Repo link added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [letters, setLetters] = useState<string>("Here will appear 'Hello react!' when you'll push button below.")
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{letters}</p>
+        <button className='button-click' onClick={() => setLetters('Hello react!')}>Show 'Hello react!'</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/maz47zu/test-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link do Repo
         </a>
       </header>
     </div>
